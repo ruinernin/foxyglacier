5456bdd6e5c756bd603bac18d9c04dfacf2947c2
ruinernin <ruinernin@mail.com>
2019-10-19 05:00:04 +0100

    Add realdebrid support

--- foxyglacier/__main__.py
+++ foxyglacier/__main__.py
@@ -2,12 +2,14 @@ __cmddocs = """foxyglacier
 
 Usage:
  foxyglacier --pm-key=<api_key> [(-y|-n)] [--wget] [DIR]
+ foxyglacier --rd-key=<api_key> [--wget] [DIR]
 
 Options:
  -y            Automatically respond yes to add
  -n            Automatically respond no to add
  --wget        Print a wget command for all links
- --pm-key=KEY  API Key
+ --pm-key=KEY  Premiumize API Key
+ --rd-key=KEY  Real-Debrid API Key
 
 """
 from pathlib import Path
@@ -27,28 +29,35 @@ def main():
         return
     torrents = [decode_torrent(torrent) for torrent in torrent_files]
     magnets = [get_magnet(torrent) for torrent in torrents]
-    pm = debrid.Premiumize(api_key=args['--pm-key'])
-    cache = pm.check_availability(magnets)
+    if args['--pm-key']:
+        ud = debrid.Premiumize(api_key=args['--pm-key'])
+    elif args['--rd-key']:
+        ud = debrid.RealDebrid(api_key=args['--rd-key'])
+    cache = ud.check_availability(magnets)
     uncached = []
     all_links = []
     for (torrent_file, magnet, cache) in zip(torrent_files, magnets, cache):
         print(f'Torrent: {torrent_file.name}')
         if cache:
             print('Cached links:')
-            links = [content['link'] for content in
-                     pm.cached_content(magnet, fn_filter=mime_check)]
+            if isinstance(cache, bool):
+                links = [content['link'] for content in
+                         ud.cached_content(magnet, fn_filter=mime_check)]
+            else:
+                links = [ud.resolve_url(magnet, cache)]
             all_links.extend(links)
             print(' \n'.join(links))
-        else:
+            continue
+        elif args['--pm-key']:
             if not (args['-y'] or args['-n']):
                 add = input('Add to Premiumize? [y/N]: ')
                 add = add.lower() == 'y'
             else:
                 add = args['-y']
             if add:
-                pm.add_torrent(str(torrent_file))
-            else:
-                uncached.append(torrent_file.name)
+                ud.add_torrent(str(torrent_file))
+                continue
+        uncached.append(torrent_file.name)
         print()
     if args['--wget']:
         print('wget', *all_links, '\n')
